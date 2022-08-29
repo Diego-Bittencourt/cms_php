4861dcf8f950189c242e147655447cc19720311b
Diego Bittencour Mendes <mendesdiego90@gmail.com>
2022-08-29 23:14:30 +0900

    connected to the database

---
+++ includes/db.php
@@ -0,0 +1,43 @@
+<?php 
+
+
+//#### the easiest way to connect to a database ####
+// $connection = mysqli_connect('localhost', 'root', '', 'cms');
+//connecting to the database, arguments are database url, username, password and which database
+//the arguments can be variables or, more secure, constants.
+
+// if ($connection) {
+//     echo "we are connected";
+// }
+//#################
+
+
+//A good way
+
+//create an array and hold the values
+$db['db_host'] = "localhost";
+$db['db_user'] = "root";
+$db['db_pass'] = "";
+$db['db_name'] = "cms";
+
+//loop through that array assigning constants.
+foreach($db as $key => $value) {
+
+
+    //creating constants thwough the array.
+    define(strtoupper($key), $value);
+
+    }
+
+    $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+    if ($connection) {
+        echo "we are connected";
+    }
+
+
+
+
+
+
+?>
