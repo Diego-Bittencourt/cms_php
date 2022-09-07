154ef7ee2829f93c4048c1ce96cd870bb049146d
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-09-07 15:08:39 +0900

    Add posts through SQL queries to the database

---
+++ admin/includes/view_all_posts.php
@@ -0,0 +1,57 @@
+
+                        <table class="table table-hover table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Author</th>
+                                    <th>Title</th>
+                                    <th>Category</th>
+                                    <th>Status</th>
+                                    <th>Image</th>
+                                    <th>Tags</th>
+                                    <th>Comments</th>
+                                    <th>Date</th>
+                                    <th>Content</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+
+<?php 
+$query = "SELECT * FROM posts";
+$select_posts = mysqli_query($connection, $query);
+
+while($row = mysqli_fetch_assoc($select_posts)) {
+    $post_id = $row['post_id'];
+    $post_author = $row['post_author'];
+    $post_title = $row['post_title'];
+    $post_category = $row['post_category_id'];
+    $post_status = $row['post_status'];
+    $post_image = $row['post_image'];
+    $post_tags = $row['post_tags'];
+    $post_comments = $row['post_comment_count'];
+    $post_date = $row['post_date'];
+    $post_content = $row['post_content'];
+
+
+
+
+
+
+
+
+                               echo "<tr>
+                                    <td>{$post_id}</td>
+                                    <td>{$post_author}</td>
+                                    <td>{$post_title}</td>
+                                    <td>{$post_category}</td>
+                                    <td>{$post_status}</td>
+                                    <td><img src='{$post_image}' height='20px' width='20px'></td>
+                                    <td>{$post_tags}</td>
+                                    <td>{$post_comments}</td>
+                                    <td>{$post_date}</td>
+                                    <td>{$post_content}</td>
+                                </tr>";
+ } ?> <!-- catching the end of the while loop. -->
+                            </tbody>
+                        </table>
+            
--- admin/posts.php
+++ admin/posts.php
@@ -12,62 +12,32 @@ include "includes/admin_header.php";
                             Welcome to Admin,
                             <small>Author</small>
                         </h1>
-                        <table class="table table-hover table-bordered">
-                            <thead>
-                                <tr>
-                                    <th>Id</th>
-                                    <th>Author</th>
-                                    <th>Title</th>
-                                    <th>Category</th>
-                                    <th>Status</th>
-                                    <th>Image</th>
-                                    <th>Tags</th>
-                                    <th>Comments</th>
-                                    <th>Date</th>
-                                    <th>Content</th>
-                                </tr>
-                            </thead>
-                            <tbody>
+
 
 <?php 
-$query = "SELECT * FROM posts";
-$select_posts = mysqli_query($connection, $query);
+//Getting the url param to include page conditionally
+if(isset($_GET['source'])) {
+    $source = $_GET['source'];
+} else {
+    $source = "";
+}
 
-while($row = mysqli_fetch_assoc($select_posts)) {
-    $post_id = $row['post_id'];
-    $post_author = $row['post_author'];
-    $post_title = $row['post_title'];
-    $post_category = $row['post_category_id'];
-    $post_status = $row['post_status'];
-    $post_image = $row['post_image'];
-    $post_tags = $row['post_tags'];
-    $post_comments = $row['post_comment_count'];
-    $post_date = $row['post_date'];
-    $post_content = $row['post_content'];
+switch($source) {
 
+    case "add_post";
+    include "includes/add_post.php";
+    break;
 
+    case "12";
+    echo "its 12";
+    break;
 
+    default:
+    include "includes/view_all_posts.php";
+}
 
+?>
 
-
-
-
-                               echo "<tr>
-                                    <td>{$post_id}</td>
-                                    <td>{$post_author}</td>
-                                    <td>{$post_title}</td>
-                                    <td>{$post_category}</td>
-                                    <td>{$post_status}</td>
-                                    <td><img src='{$post_image}' height='20px' width='20px'></td>
-                                    <td>{$post_tags}</td>
-                                    <td>{$post_comments}</td>
-                                    <td>{$post_date}</td>
-                                    <td>{$post_content}</td>
-                                </tr>";
- } ?> <!-- catching the end of the while loop. -->
-                            </tbody>
-                        </table>
-            
 
             <div class="container-fluid">
 
