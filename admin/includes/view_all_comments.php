8fbdb465bc42b8b2bd1e4316c789483e616cc338
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-09-15 09:15:31 +0900

    update the comment table with more data

--- admin/includes/view_all_comments.php
+++ admin/includes/view_all_comments.php
@@ -4,10 +4,14 @@
                                 <tr>
                                     <th>Id</th>
                                     <th>Author</th>
+                                    <th>Comment</th>
                                     <th>Email</th>
-                                    <th>Content</th>
-                                    <th>Date</th>
                                     <th>Status</th>
+                                    <th>In response to</th>
+                                    <th>Date</th>
+                                    <th>Approve</th>
+                                    <th>Unapprove</th>
+                                    <th>Delete</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -25,6 +29,13 @@ while($row = mysqli_fetch_assoc($select_comments)) {
     $comment_content = $row['comment_content'];
     $comment_status = $row['comment_status'];
 
+$find_post_query = "SELECT * FROM posts WHERE post_id = '{$comment_post_id}' ";
+$select_post_id = mysqli_query($connection, $find_post_query);
+
+while($row = mysqli_fetch_assoc($select_post_id)) {
+    $post_id = $row['post_id'];
+    $post_title = $row['post_title'];
+}
 
 
     
@@ -32,10 +43,14 @@ while($row = mysqli_fetch_assoc($select_comments)) {
                                echo "<tr>
                                     <td>{$comment_id}</td>
                                     <td>{$comment_author}</td>
-                                    <td>{$comment_email}</td>
                                     <td>{$comment_content}</td>
-                                    <td>{$comment_date}</td>
+                                    <td>{$comment_email}</td>
                                     <td>{$comment_status}</td>
+                                    <td>{$post_title}</td>
+                                    <td>{$comment_date}</td>
+                                    <td><a href=''>Approve</a></td>
+                                    <td><a href=''>Unapprove</a></td>
+                                    <td><a href=''>Delete</a></td>
                                     </tr>";
  } ?> <!-- catching the end of the while loop. -->
                             </tbody>
